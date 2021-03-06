b13966bb25d502899e49f84c16179fdd8ca0cf10
Anna Bowers <eenabow@gmail.com>
2021-03-06 17:46:21 -0500

    Adding notes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -14,3 +14,19 @@ d3.json("samples.json").then(function(sampleData) {
 sampleData.html("");
 
 };
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+    // Use sample_values as the values for the bar chart.
+    // Use otu_ids as the labels for the bar chart.
+    // Use otu_labels as the hovertext for the chart.
+
+// Create a bubble chart that displays each sample.
+    // Use otu_ids for the x values.
+    // Use sample_values for the y values.
+    // Use sample_values for the marker size.
+    // Use otu_ids for the marker colors.
+    // Use otu_labels for the text values.
+
+
+
+// Demographic Info- Display each key-value pair from the metadata JSON object somewhere on the page.
+
