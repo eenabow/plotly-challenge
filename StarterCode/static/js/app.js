887b26ae63aa12bd72ad60b702e513227ab7d448
Anna Bowers <eenabow@gmail.com>
2021-03-08 20:50:08 -0500

    Added code for selecting id

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,32 +1,75 @@
 function getData(id) {
-// Fetch the JSON data and console log it
-d3.json("samples.json").then(function(sampleData) {
-  console.log(sampleData);
-  var sample_values = sampleData.samples[0].sample_values.slice(0,10).reverse();
-  console.log(sampleValues);
-  var otu_ids = sampleData.samples[0].otu_ids;
-  console.log(otu_ids);
-  var otu_labels = sampleData.samples[0].otu_labels.slice(0,10);
-  console.log(otu_labels)
-});
+    // Fetch the JSON data and console log it
+    d3.json("samples.json").then(function (sampleData) {
+        console.log(sampleData);
+        var sample_values = sampleData.samples[0].sample_values.slice(0, 10).reverse();
+        console.log(sampleValues);
+        var otu_ids = sampleData.samples[0].otu_ids;
+        console.log(otu_ids);
+        var otu_labels = sampleData.samples[0].otu_labels.slice(0, 10);
+        console.log(otu_labels)
+    });
 
-// Clear any existing sample data 
-sampleData.html("");
+    // Clear any existing sample data 
+    sampleData.html("");
 
 };
+console.log(getData)
+
+// locate the test subject id 
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-    // Use sample_values as the values for the bar chart.
-    // Use otu_ids as the labels for the bar chart.
-    // Use otu_labels as the hovertext for the chart.
+// Use sample_values as the values for the bar chart.
+// Use otu_ids as the labels for the bar chart.
+// Use otu_labels as the hovertext for the chart.
 
 // Create a bubble chart that displays each sample.
-    // Use otu_ids for the x values.
-    // Use sample_values for the y values.
-    // Use sample_values for the marker size.
-    // Use otu_ids for the marker colors.
-    // Use otu_labels for the text values.
+// Use otu_ids for the x values.
+// Use sample_values for the y values.
+// Use sample_values for the marker size.
+// Use otu_ids for the marker colors.
+// Use otu_labels for the text values.
 
 
 
 // Demographic Info- Display each key-value pair from the metadata JSON object somewhere on the page.
+function populateDemographic(optionSelected) {
+    d3.json("samples.json").then(function (samplesData) {
+        var demoInfo = samplesData.metadata
+        var filteredDemo = demoInfo.filter( subject => subject.id == optionSelected)
+        // var filteredDemo = demoInfo.filter
+        console.log(filteredDemo)
+        var demoList = d3.select("#sample-metadata")
 
+// append a p tag, a text value, etc. 
+    });
+};
+
+// "id": 940,
+// "ethnicity": "Caucasian",
+// "gender": "F",
+// "age": 24.0,
+// "location": "Beaufort/NC",
+// "bbtype": "I",
+// "wfreq": 2.0
+
+// Function to select ID 
+function populateSelector() {
+    var idSelector = d3.select("#selDataset");
+    d3.json("samples.json").then(function (samplesData) {
+        var idNames = samplesData.names
+        idNames.forEach((name) => {
+            idSelector.append("option").text(name).property("value", name)
+        })
+    });
+};
+
+
+// Update all of the plots any time that a new sample is selected.
+function optionChanged(optionSelected) {
+    populateDemographic(optionSelected)
+
+};
+
+
+
+populateSelector();
